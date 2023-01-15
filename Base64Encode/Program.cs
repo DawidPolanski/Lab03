1e090f6491711d23c64b87b3918116a77e722009
ASUS-REZERWOWY-\dawid <dpolanski1999@gmail.com>
2023-01-15 13:40:44 +0100

    Password checker & encode to Base64

---
+++ Base64Encode/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.ForegroundColor = ConsoleColor.Green;
+while(true)
+{
+    Console.Write("Enter string to encode base64: ");
+    string stringinput = Console.ReadLine();
+    string encodeString = Base64Encode(stringinput);
+    Console.Write("Your input {0} encode to base64 {1}", stringinput, encodeString);
+    Console.ReadKey();
+    Console.Clear();
+}
+
+string Base64Encode(string text)
+{
+    var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(text);
+    return System.Convert.ToBase64String(plainTextBytes);
+}
---
+++ PasswordChecker/Program.cs
@@ -0,0 +1,60 @@
+﻿while(true)
+{
+    Console.Write("Enter password: ");
+    string pass = Console.ReadLine();
+    int passLength = pass.Length;
+
+    if (passLength >= 8)
+    {
+        bool containsAtLeastOneUppercase = pass.Any(char.IsUpper);
+        if (containsAtLeastOneUppercase == true)
+        {
+            bool containsAtLeastOneNum = pass.Any(char.IsNumber);
+            if (containsAtLeastOneNum == true)
+            {
+                bool containsAtLeastOneChar = pass.Any(char.IsSymbol);
+                if (containsAtLeastOneChar == true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Your password is safety");
+                    Console.ReadKey();
+                    Console.ResetColor();
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error please enter char");
+                    Console.ReadKey();
+                    Console.ResetColor();
+                    Console.Clear();
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Error please enter number!");
+                Console.ReadKey();
+                Console.ResetColor();
+                Console.Clear();
+            }
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Error please enter upercase!");
+            Console.ReadKey();
+            Console.ResetColor();
+            Console.Clear();
+        }
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Your password isn't safety you must input minimal 8 characters");
+        Console.ReadKey();
+        Console.ResetColor();
+        Console.Clear();
+    }
+}
+
